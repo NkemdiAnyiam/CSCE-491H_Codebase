53e2ef3fa3ac452c979ed16a692194d2981c7106
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-02-09 15:15:11 -0600

    Reference SVG element in AnimBlockLine while still passing in line element to super constructor

--- scripts/AnimBlockLine.js
+++ scripts/AnimBlockLine.js
@@ -6,15 +6,16 @@ export class AnimBlockLine extends AnimBlock {
   updateEndpointsOnEntry = true; // determines whether or not to call updateEndpoints() upon using an entering animation
   trackEndpoints = false; // determines whether or not to continuously periodically called updateEndpoints() while visible
 
-  constructor(domElem, animName, startElem, [leftStart, topStart], endElem, [leftEnd, topEnd], options) {
-    super(domElem, animName, options);
+  constructor(domSVGElem, animName, startElem, [leftStart, topStart], endElem, [leftEnd, topEnd], options) {
+    super(domSVGElem.querySelector('.free-line__line'), animName, options);
 
-    AnimBlockLineUpdater.registerDomElem(this.domElem); // enables any AnimBlockLines using the same DOM element as us to effectively toggle the continuous updates
+    this.domSVGElem = domSVGElem;
+    AnimBlockLineUpdater.registerDomElem(this.domSVGElem); // enables any AnimBlockLines using the same DOM element as us to effectively toggle the continuous updates
 
     // set the reference points for the start and end of the line (our <svg> element's nested <line>).
     // Defaults to the sibling DOM element above our DOM element
-    this.startElem = startElem ? startElem : this.domElem.previousElementSibling;
-    this.endElem = endElem ? endElem : this.domElem.previousElementSibling;
+    this.startElem = startElem ? startElem : this.domSVGElem.previousElementSibling;
+    this.endElem = endElem ? endElem : this.domSVGElem.previousElementSibling;
 
     // set the values used for the endpoint offsets relative to the top-left of each reference element
     this.leftStart = leftStart; // a value of 0 leaves the starting endpoint on the left edge of startElem. 0.5 centers it horizontally within startElem
@@ -58,11 +59,11 @@ export class AnimBlockLine extends AnimBlock {
       if (this.updateEndpointsOnEntry) { this.updateEndpoints(); }
 
       // if continuous tracking is enabled, tell AnimBlockLineUpdater to set an interval for updateEndpoints()
-      if (this.trackEndpoints) { AnimBlockLineUpdater.setInterval(this.domElem, this.updateEndpoints.bind(this)); }
+      if (this.trackEndpoints) { AnimBlockLineUpdater.setInterval(this.domSVGElem, this.updateEndpoints.bind(this)); }
     }
 
     // if we are exiting, turn off the interval for updateEndPoints()
-    if (AnimBlock.isExiting(animName)) { AnimBlockLineUpdater.clearInterval(this.domElem); }
+    if (AnimBlock.isExiting(animName)) { AnimBlockLineUpdater.clearInterval(this.domSVGElem); }
   }
 
   updateEndpoints() {
@@ -70,19 +71,19 @@ export class AnimBlockLine extends AnimBlock {
     // get the bounding rectangles for starting reference element, ending reference element, and parent element
     const rectStart = this.startElem.getBoundingClientRect();
     const rectEnd = this.endElem.getBoundingClientRect();
-    const rectParent = this.domElem.parentElement.getBoundingClientRect();
+    const rectParent = this.domSVGElem.parentElement.getBoundingClientRect();
 
     // The x and y coordinates of the line need to be with respect to the top left of document
     // Thus, we must subtract the parent element's current top and left from the offset
     // But because elements start in their parent's Content box—which excludes the border—...
     // ...instead of the Fill area—which includes the border—,our element's top and left are offset by the parent element's border width...
     // ... with respect to the actual bounding box of the parent. Therefore, we must subtract the parent's border thicknesses as well.
-    const SVGLeftOffset = -rectParent.left - Number.parseFloat(getComputedStyle(this.domElem.parentElement).borderLeftWidth);
-    const SVGTopOffset = -rectParent.top - Number.parseFloat(getComputedStyle(this.domElem.parentElement).borderTopWidth);
+    const SVGLeftOffset = -rectParent.left - Number.parseFloat(getComputedStyle(this.domSVGElem.parentElement).borderLeftWidth);
+    const SVGTopOffset = -rectParent.top - Number.parseFloat(getComputedStyle(this.domSVGElem.parentElement).borderTopWidth);
 
     // change the x and y coordinates of our <svg>'s nested <line> based on the bounding boxes of the starting and ending reference elements
     // the offset with respect to the reference elements' tops and lefts is calculated using linear interpolation
-    const line = this.domElem.querySelector('.free-line__line');
+    const line = this.domSVGElem.querySelector('.free-line__line');
     line.x1.baseVal.value = (1 - this.leftStart) * rectStart.left + (this.leftStart) * rectStart.right + SVGLeftOffset;
     line.y1.baseVal.value = (1 - this.topStart) * rectStart.top + (this.topStart) * rectStart.bottom + SVGTopOffset;
     line.x2.baseVal.value = (1 - this.leftEnd) * rectEnd.left + (this.leftEnd) * rectEnd.right + SVGLeftOffset;
