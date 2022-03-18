26566273d0069ed1cb7d828888a384a6b703583a
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-03-18 18:59:18 -0500

    Add stof() function

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,2 +1,3 @@
 export const wait = milliseconds => new Promise(resolve => setTimeout(resolve, milliseconds));
 export const stoi = string => Number.parseInt(string);
+export const stof = string => Number.parseFloat(string);
